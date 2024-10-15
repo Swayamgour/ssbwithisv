8f67cd5d763aebf92baeeb68fa2837dd60c3a057
aryantony <aryanroy386@gmail.com>
2024-10-15 20:38:13 +0530

    added

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,23 +1,14 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  // Check if the MONGO_URL is defined
-  if (!process.env.MONGO_URL) {
-    console.error("MongoDB connection string is not defined in environment variables.");
-    process.exit(1); // Exit the process with failure
-  }
-
-  try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true, // New connection management engine
-      useCreateIndex: true, // Use createIndex() instead of ensureIndex()
-    });
-    console.log("MongoDB connection: success");
-  } catch (err) {
-    console.error("MongoDB connection error:", err.message);
-    process.exit(1); // Exit the process with failure
-  }
+const connectDB = () => {
+    mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true, // It's a good practice to use this option
+    })
+    .then(() => {
+        console.log("MongoDB connection: success");
+    })
+    .catch((err) => console.log(err));
 };
 
 module.exports = { connectDB };
